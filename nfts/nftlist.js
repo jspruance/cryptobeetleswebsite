a8fd42f609dcf02461d01bf771ba0f7855539f19
Jonathan Spruance <jspruance@inflightcorp.com>
2021-12-02 17:17:04 +0700

    Adding new nft drop

--- nfts/nftlist.js
+++ nfts/nftlist.js
@@ -35,13 +35,33 @@ const nftlist = [
       {
         name: 'cryptobeetle_171',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/7',
-        order: '6'
+        order: '7'
       },
       {
         name: 'cryptobeetle_187',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/8',
-        order: '6'
+        order: '8'
       },
+      {
+        name: 'cryptobeetle_024',
+        link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/15',
+        order: '9'
+      },
+      {
+        name: 'cryptobeetle_185',
+        link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/11',
+        order: '10'
+      },
+      {
+        name: 'cryptobeetle_193',
+        link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/12',
+        order: '11'
+      },
+      {
+        name: 'cryptobeetle_190',
+        link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/13',
+        order: '12'
+      }
     ]
   }
 ]

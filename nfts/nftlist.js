7181d9aafb89e628754d66a6a167a46fd8579a39
Jonathan Spruance <jspruance@inflightcorp.com>
2021-12-06 19:33:19 +0700

    Add NFTs

--- nfts/nftlist.js
+++ nfts/nftlist.js
@@ -48,70 +48,76 @@ const nftlist = [
         order: '7'
       },
       {
+        name: 'cryptobeetle_216',
+        extension: 'png',
+        link: '',
+        order: '8'
+      },
+      {
         name: 'cryptobeetle_060',
         extension: 'gif',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/1',
-        order: '8'
+        order: '9'
       },
       {
         name: 'cryptobeetle_075',
         extension: 'gif',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/4',
-        order: '9'
+        order: '10'
       },
       {
         name: 'cryptobeetle_069',
         extension: 'gif',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/2',
-        order: '10'
+        order: '11'
       },
       {
         name: 'cryptobeetle_022',
         extension: 'gif',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/3',
-        order: '11'
+        order: '12'
       },
       {
         name: 'cryptobeetle_185',
         extension: 'gif',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/11',
-        order: '12'
+        order: '13'
       },
       {
         name: 'cryptobeetle_187',
         extension: 'gif',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/8',
-        order: '13'
+        order: '14'
       },
       {
         name: 'cryptobeetle_024',
         extension: 'gif',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/15',
-        order: '14'
+        order: '15'
       },
       {
         name: 'cryptobeetle_193',
         extension: 'gif',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/12',
-        order: '15'
+        order: '16'
       },
       {
         name: 'cryptobeetle_190',
         extension: 'gif',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/13',
-        order: '16'
+        order: '17'
       },
       {
         name: 'cryptobeetle_171',
         extension: 'gif',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/7',
-        order: '17'
+        order: '18'
       },
       {
         name: 'cryptobeetle_116',
         extension: 'gif',
         link: 'https://opensea.io/assets/0xfb2ce8f578d11a4d6cbae57151bb0cc2e32a3562/6',
-        order: '18'
+        order: '19'
       }
     ]
   }
